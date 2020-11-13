ee6ea6ff1919d7fc50bcd1a2192faecff726e0b0
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-13 14:30:32 +0300

    Models,Views And Controller DEMO
    Models,Views And Controller DEMO

---
+++ e-commerce-DEMO/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using e_commerce_DEMO.Models.Product;
+
+namespace e_commerce_DEMO.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAll()
+        {
+            ViewData["Heading"] = "Tüm ürünler";
+            var products = new List<Product>();
+            products.Add(new Product { ID = 101, Name = "Apple", Price = 1.1 });
+            products.Add(new Product { ID = 202, Name = "Bike", Price = 2.2 });
+            products.Add(new Product { ID = 303, Name = "Calculator", Price = 3.3 });
+
+            products.Add(new Product { ID = 122, Name = "Çaklıt", Price = 4.4 });
+            return View(products);
+        }
+    }
+}
---
+++ e-commerce-DEMO/Models/Members.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_DEMO.Models
+{
+    public class Members{
+        public int ID;
+        public string EMAIL;
+        public int PASS;
+        public string NAME;
+        public string SURNAME;
+        public DateTime BİRHTDAY;
+        public string AUTH;
+
+
+    }
+}

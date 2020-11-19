7e6eee42b3d01ee9a5ce69b1c7863a12e0270d6b
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-19 20:16:23 +0300

    Homework

---
+++ e-commerce-DEMO/Controllers/HomeworkController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_DEMO.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Example1()
+        {
+            TempData["welcomeMessage"] ="Kanalıma Hoşgeldiniz Abone olmayı unutmayın.";
+            TempData["subsCount"] = 200.000;
+            TempData["buttonName"] = "Abone Ol";
+            TempData["buttonColor"] = "#b40505";
+            return View();
+        }
+        public IActionResult Example2()
+        {
+            ViewBag.welcomeMessage = "Kanalıma Hoşgeldiniz Abone olmayı unutmayın.";
+            ViewBag.subsCount = 200.000;
+            ViewBag.buttonName = "Abone Ol";
+            ViewBag.buttonColor = "#b40505";
+            return View();
+        }
+        public IActionResult Example3()
+        {
+            ViewData["welcomeMessage"] = "Kanalıma Hoşgeldiniz Abone olmayı unutmayın.";
+            ViewData["subsCount"] = 200.000;
+            ViewData["buttonName"] = "Abone Ol";
+            ViewData["buttonColor"] = "#b40505";
+            return View();
+        }
+    }
+}
